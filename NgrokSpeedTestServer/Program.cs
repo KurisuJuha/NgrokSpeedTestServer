73a2be1ac8b7c6705f6be764a772cd75303eea86
KYapp <kukiyomenaisapp@gmail.com>
2023-02-18 22:21:17 +0900

    Update Program.cs

--- NgrokSpeedTestServer/Program.cs
+++ NgrokSpeedTestServer/Program.cs
@@ -1,45 +1,26 @@
 ﻿using WebSocketSharp;
 using WebSocketSharp.Server;
 
-namespace NgrokSpeedTestServer;
 
-internal class Program
+Console.WriteLine("Hello, World!");
+var webSocketServer = new WebSocketServer(8080);
+webSocketServer.AddWebSocketService<Server>("/");
+webSocketServer.Start();
+while (true)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        var webSocketServer = new WebSocketServer(8080);
-        webSocketServer.AddWebSocketService<Server>("/");
-        webSocketServer.Start();
-        while (true)
-        {
-            var m = Console.ReadLine();
-            if (m == "quit") break;
-        }
-        webSocketServer.Stop();
-    }
+    var m = Console.ReadLine();
+    if (m == "quit") break;
 }
+webSocketServer.Stop();
 
 public class Server : WebSocketBehavior
 {
     protected override void OnClose(CloseEventArgs e)
-    {
-        base.OnClose(e);
-        Console.WriteLine("close");
-    }
+        => Console.WriteLine("close");
     protected override void OnOpen()
-    {
-        base.OnOpen();
-        Console.WriteLine("open");
-    }
+        => Console.WriteLine("open");
     protected override void OnError(WebSocketSharp.ErrorEventArgs e)
-    {
-        base.OnError(e);
-        Console.WriteLine(e.Message);
-        Console.WriteLine("error");
-    }
+        => Console.WriteLine("error");
     protected override void OnMessage(MessageEventArgs e)
-    {
-        Send(e.RawData);
-    }
+        => Send(e.RawData);
 }
