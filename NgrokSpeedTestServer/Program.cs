882f77053176edb1ae67b1c3475d7a0e3c3fd225
KYapp <kukiyomenaisapp@gmail.com>
2023-02-22 16:16:04 +0900

    Update Program.cs

--- NgrokSpeedTestServer/Program.cs
+++ NgrokSpeedTestServer/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using System.Net;
+using System.Text;
+using System.Net.WebSockets;
+using System.Net.Sockets;
+
+TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 3000);
+listener.Start();
+Socket socket = listener.AcceptSocket();
